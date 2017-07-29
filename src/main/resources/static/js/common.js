5ec0bd87618eb827a88ac24797cad6d5dabb3cff
Jonathan Pecoraro <JonPecoraro@gmail.com>
2017-07-29 14:33:59 -0400

    added code to redirect HTTP requests to HTTPS

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -22,7 +22,34 @@ common.isValid = function($parentElement) {
 	return valid;
 };
 
+common.toggleMenuHover = function() {
+	// remove the hover capability if screen size is too small.
+	if (!$('#nav .navbar-toggle').is(':visible')) {
+		$('#navbar li.dropdown').hover(function() {
+		   $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(250); 
+		}, function() {
+			$(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(250);
+		});
+	} else {
+		$('#navbar li.dropdown').unbind('mouseenter mouseleave')
+	}
+}
+
+common.httpsRedirect() {
+	// Redirects all HTTP request to the HTTPS website
+	if (location.protocol == 'http:' && location.href.indexOf('murraywhiteins') > -1) {
+		location.href = location.href.replace('http:', 'https:');
+	}
+}
+
 $(function() {
+	// Redirect HTTP requests to HTTPS website
+	common.httpsRedirect();
+	
+	// Enable/Disable menu hover based on screen size
+	common.toggleMenuHover();
+	$(window).on('resize', common.toggleMenuHover);
+	
 	// Turn all inputs with the dateField class into date pickers
 	$('input.dateField').datepicker();
 });
